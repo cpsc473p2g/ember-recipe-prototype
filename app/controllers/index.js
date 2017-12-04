3a91b1ce638eba476a7f050f0b66824c1094cb80
Philip Chung <philipchung1995@yahoo.com>
2017-12-03 19:10:24 -0800

    Move search query to results route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,11 +3,11 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions:{
     search: function(qValue){
-      var results = this.get('store').query('recipe',{
-        q: qValue
+      this.transitionToRoute('results', {
+        queryParams: {
+          q: qValue
+        }
       });
-      this.transitionToRoute('results', results)
     }
-    
   }
 });
