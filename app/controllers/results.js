d6de8700d3744704ffb2f98030eb994d17e78717
shelleypham <phamshelley@csu.fullerton.edu>
2017-12-04 17:44:02 -0800

    screw it

--- app/controllers/results.js
+++ app/controllers/results.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	queryParams: ['q'],
 	actions:{
-    add: function(r_name, r_sitename, r_yield, r_source,r_image,r_ingredient,r_attr){
+    add: function(r_name, r_sitename, r_yield, r_source,r_image,r_ingredient,r_attr, r_uid){
       var r_name1 = r_name
 			var r_sitename1 = r_sitename
 			var r_yield1 = r_yield
@@ -11,21 +11,22 @@ export default Controller.extend({
 			var r_image1 = r_image
 			var r_ingredient1 = r_ingredient
 			var r_attr1 = r_attr
+			var uid1 = r_uid
 
-			alert(r_name)
-			alert(r_yield)
+			alert('Added to favorites');
       //create new favorite
       var newFavorite = this.store.createRecord('favorite',{
         title: r_name1,
 				sitename: r_sitename1,
 				source: r_source1,
-				image: r_image1
+				image: r_image1,
+				uid: uid1
       });
 
       //save favorite to database
       newFavorite.save();
 
-
+			
     }
   }
 });
--- app/models/favorite.js
+++ app/models/favorite.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   sitename: DS.attr('string'),
   source: DS.attr('string'),
-  image: DS.attr('string')
+  image: DS.attr('string'),
+  uid: DS.attr('string')
 });
