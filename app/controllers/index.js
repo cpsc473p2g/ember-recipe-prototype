c3a1dc14fb1467ee4514325f0dded9ee5a4fb008
Philip Chung <philipchung1995@yahoo.com>
2017-12-04 17:53:46 -0800

    Add allergies to search

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  query: null,
+  allergens: [],
   actions:{
-    search: function(qValue){
+    search: function(){
       this.transitionToRoute('results', {
         queryParams: {
-          q: qValue
+          'q': this.get('query'),
+          'allowedAllergy[]': this.get('allergens').mapBy('value'),
         }
       });
     }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
+import { inject } from '@ember/service';
 
 export default Route.extend({
+  yummlyDicts: inject(),
+  model() {
+    return Ember.RSVP.hash({
+      allergens: this.get('yummlyDicts').allergies(),
+    });
+  }
 });
